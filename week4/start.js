c791355f28bb651a0710656a7ae0b7d67f71bae8
Romaric Zounlome <rzounlom@gmail.com>
2022-10-27 18:08:12 -0400

    complete week 4 lesson plan

---
+++ week4/start.js
@@ -0,0 +1,10 @@
+//**Exercises */
+
+/*1. using an arrow function, create a function called reverseString that takes in a string (str),
+ and returns the reverse of the string, use a for loop, or for in loop /*
+
+/*2. using arrow function, create a function called factorialize accepts a number, (num),
+ and returns the factorial of said number. example 5 fatorial is 5 * 4 * 3 * 2 * 1 = 120/*
+
+/*3. using an arrow function, create a function that takes in a name argument, 
+and a callback function which returns a greeting using the name from the parent function, and returns a*/
