3aa97237ae4ad125a367dd28e60c38003ee312c7
Romaric Zounlome <rzounlom@gmail.com>
2022-10-31 22:04:45 -0400

    complete Errors lesson plan

--- week5/Errors/start.js
+++ week5/Errors/start.js
@@ -1,10 +1,10 @@
 const axios = require("axios");
 
-/** Promises */
+/** Handling Errors/Promises */
 /* The Promise object represents the eventual completion (or failure) of an 
 asynchronous operation and its resulting value. */
 const myPromise = () => {
-  const data = axios
+  axios
     .get("http:localhost:5000")
     .then((data) => console.log(data))
     .catch((err) => console.log(err));
@@ -12,19 +12,61 @@ const myPromise = () => {
 
 // myPromise();
 
+const getUsers = () => {
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos")
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err.message));
+};
+
+// console.log(getUsers());
+
+const getUsersError = () => {
+  axios
+    .get("https://jsonplaceholder.typicode.com/todo")
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err.message));
+};
+
+// console.log(getUsersError());
+
+const getUserById = (id = 1) => {
+  axios
+    .get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err.message));
+};
+
+// console.log(getUserById()); //should return user with id 1 as default
+// console.log(getUserById(5));//should return user with id 4
+
 //async await
-const myAsyncFunction = async () => {
+const getUsersAsyncAwait = async () => {
   try {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/todos"
     );
     console.log(response);
   } catch (error) {
-    console.error(error);
+    throw new Error(error.message);
   }
 };
 
-// console.log(myAsyncFunction());
+// console.log(getUsersAsyncAwait());
+
+//async await error case
+const getUsersAsyncAwaitError = async () => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todo"
+    );
+    console.log(response);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+// console.log(getUsersAsyncAwaitError());
 
 //**try catch finally example */
 
@@ -45,4 +87,4 @@ function get() {
   return 40;
 }
 
-console.log("The value is ", get());
+// console.log("The value is ", get());
