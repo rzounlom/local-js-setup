472df13962d019ca6573801508144a8667b97b92
Romaric Zounlome <rzounlom@gmail.com>
2022-11-21 22:29:12 -0500

    minor change to css styles comments

--- week8/portfolio_complete/js/index.js
+++ week8/portfolio_complete/js/index.js
@@ -1,11 +1,15 @@
+// Add callback when page is scrolled
 window.onscroll = function () {
   scrollFunction();
 };
 
+//scroll callback function
 function scrollFunction() {
+  // when the user scrolls past 20px we add the scroll-background class to the navbar to change its background color
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.querySelector("nav").classList.add("scroll-background");
   } else {
+    // when the user scrolls less than 20px we remove the scroll-background class from the navbar to change its background color to the default
     document.querySelector("nav").classList.remove("scroll-background");
   }
 }
