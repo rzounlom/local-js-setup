829d024e8bc8b427293a4c169b8afa24440719f7
Romaric Zounlome <rzounlom@gmail.com>
2022-12-03 13:53:10 -0500

    complete tablet styles

--- week8/portfolio_complete/js/index.js
+++ week8/portfolio_complete/js/index.js
@@ -8,6 +8,7 @@ window.onscroll = function () {
 const navScroll = () => {
   // when the user scrolls past 20px we add the scroll-background class to the navbar to change its background color
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    console.log("BODY", document.body.scrollTop);
     document.querySelector("nav").classList.add("scroll-background");
   } else {
     // when the user scrolls less than 20px we remove the scroll-background class from the navbar to change its background color to the default
