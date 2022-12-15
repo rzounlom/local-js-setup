8b3f630d11e7d9fde15e7ca1c9ac006f93d04ed9
Romaric Zounlome <rzounlom@gmail.com>
2022-12-15 07:58:54 -0500

    add todo app

---
+++ week10/todo-app-bootstrap/index.js
@@ -0,0 +1,69 @@
+//store id in variable
+let id = 0;
+
+//get id of button
+document.getElementById("add").addEventListener("click", () => {
+  //get current date/time
+  let createdDate = new Date();
+  //get reference to table to add data
+  let table = document.getElementById("list");
+  // console.log("TABLE", table);
+  //get reference to the row to add new data
+  let row = table.insertRow(1);
+  //add id to the new row and set it to the current id variable above
+  row.setAttribute("id", `item-${id}`);
+  //get reference to task and date inputs
+  let taskInput = document.getElementById("new-task");
+  let startDateInput = document.getElementById("new-start-date");
+  let endDateInput = document.getElementById("new-end-date");
+  // console.log({ taskInput, startDateInput, endDateInput });
+  //alert if not all inputs have values
+  const inputsValidated =
+    taskInput.value && startDateInput.value && endDateInput.value;
+  if (!inputsValidated) {
+    alert(`Error creating a note. Please make sure:
+    • A Start Date is provided
+    • A Start Date is provided
+    • A Todo is provided
+    `);
+    return;
+  }
+  //set task equal to the text entered into the task input
+  row.insertCell(0).innerHTML = taskInput.value;
+  //set Date Created cell to createdDate variable above with date formatting: YYYY/MM/DD
+  row.insertCell(1).innerHTML = `${createdDate.getFullYear()}-${
+    createdDate.getMonth() + 1
+  }-${createdDate.getDate()}`;
+  //set Start Date cell to Start Date date picker
+  row.insertCell(2).innerHTML = startDateInput.value;
+  //set End Date cell to End Date date picker
+  row.insertCell(2).innerHTML = endDateInput.value;
+  //add buttons(actions) to Actions cell
+  let actions = row.insertCell(4);
+  actions.appendChild(createDeleteButton(id++));
+  //reset the task and date inputs to text
+  taskInput.value = "";
+  startDateInput.value = "";
+  endDateInput.value = "";
+});
+
+const createDeleteButton = (id) => {
+  //create new button to return
+  const btn = document.createElement("button");
+  //add bootstrap class to button
+  btn.className = "btn btn-danger";
+  //add id to button
+  btn.id = id;
+  //add text to button
+  btn.innerHTML = `Delete`;
+  //add click handler to button for delete action
+  btn.onclick = () => {
+    console.log(`Deleting row with id: item-${id}`);
+    //get reference to row we want to delete
+    const elementToDelete = document.getElementById(`item-${id}`);
+    //remove child from parent node
+    elementToDelete.parentNode.removeChild(elementToDelete);
+  };
+  //return button
+  return btn;
+};
