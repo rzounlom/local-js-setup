6ff5d093929b701e955a1fd50fb042f77386f485
Romaric Zounlome <rzounlom@gmail.com>
2023-01-19 15:22:50 -0500

    add week13 cra examples

---
+++ week13/create-react-app/my-app-func-components-start/src/components/comment.js
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import LikeButton from "./like-button";
+import ReplyButton from "./reply-button";
+
+export default class Comment extends Component {
+  render() {
+    return (
+      <div className="card w-75">
+        <div className="card-header bg-success text-white">
+          Username and Time
+        </div>
+        <div className="card-body">Comment Content</div>
+        <div className="card-footer">
+          <div>
+            <LikeButton />
+            <span> </span>
+            <ReplyButton />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
