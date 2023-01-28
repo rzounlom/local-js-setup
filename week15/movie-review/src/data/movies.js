8fda1adc1db921cba69236544686be1ba651f352
Romaric Zounlome <rzounlom@gmail.com>
2023-01-28 17:13:27 -0500

    scafold movie app

---
+++ week15/movie-review/src/data/movies.js
@@ -0,0 +1,89 @@
+export const movies = [
+  {
+    id: 1,
+    name: "Shotgun Wedding",
+    imgUrl:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcST4eAYLJlEMgcyTdsbteeHsyYWfZqjAgGH8UzOmmtYP3-QtwEG",
+    summary:
+      "Darcy and Tom gather their families for a destination wedding, but the ceremony gets put on hold when gunmen take everyone hostage. Now, they must do everything they can to save their loved ones -- if they don't wind up killing each other first.",
+    rating: 4.5,
+    ratings: [
+      {
+        id: 1,
+        review: "Best movie ever!",
+        rating: 5.0,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Everything Everywhere All at Once",
+    imgUrl:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSHXnPFszcpNapxaCWUSBl3LZezeb-9X_HUKcefCmNZfbPZzmZ5",
+
+    summary:
+      "When an interdimensional rupture unravels reality, an unlikely hero must channel her newfound powers to fight bizarre and bewildering dangers from the multiverse as the fate of the world hangs in the balance.",
+    rating: 4.0,
+    ratings: [
+      {
+        id: 1,
+        review: "Best movie ever!",
+        rating: 5,
+      },
+      {
+        id: 2,
+        review: "Average at best",
+        rating: 3,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "M3GAN",
+    imgUrl:
+      "https://dx35vtwkllhj9.cloudfront.net/universalstudios/megan/images/regions/us/onesheet.jpg",
+
+    summary:
+      "M3GAN is a marvel of artificial intelligence, a lifelike doll that's programmed to be a child's greatest companion and a parent's greatest ally. Designed by Gemma, a brilliant roboticist, M3GAN can listen, watch and learn as it plays the role of friend and teacher, playmate and protector. When Gemma becomes the unexpected caretaker of her 8-year-old niece, she decides to give the girl an M3GAN prototype, a decision that leads to unimaginable consequences.",
+    rating: 4.0,
+    ratings: [
+      {
+        id: 1,
+        review: "Best movie ever!",
+        rating: 5,
+      },
+      {
+        id: 2,
+        review: "Scary as hell!",
+        rating: 4,
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Black Panther: Wakanda Forever",
+    imgUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTOZ_Qmjk5-MJ8ZV3Pygz1RvQhO1g_1POlHrtnYtEbM6VMx3d7y",
+
+    summary:
+      "Queen Ramonda, Shuri, M'Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T'Challa's death. As the Wakandans strive to embrace their next chapter, the heroes must band together with Nakia and Everett Ross to forge a new path for their beloved kingdom.",
+    rating: 4.0,
+    ratings: [
+      {
+        id: 1,
+        review: "Best movie ever!",
+        rating: 5,
+      },
+      {
+        id: 2,
+        review: "Love th ending!",
+        rating: 4.5,
+      },
+      {
+        id: 3,
+        review: "The story was very compelling",
+        rating: 5,
+      },
+    ],
+  },
+];
