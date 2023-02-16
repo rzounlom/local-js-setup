8afc0627e059a00e5cc1831d19c2d1b01b36cb69
Romaric Zounlome <rzounlom@gmail.com>
2023-02-16 10:18:53 -0500

    add basic login form

--- week17/easy-hr/src/features/auth/login/Login.js
+++ week17/easy-hr/src/features/auth/login/Login.js
@@ -1,3 +1,93 @@
-export default function Login() {
-  return <div>Login</div>;
-}
+import "./Login.scss";
+
+import { Button, Checkbox, Form, Input } from "antd";
+
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="login-heading">
+        <h1>Login</h1>
+        <Link to="/">
+          <Button
+            type="primary"
+            style={{ background: "aqua" }}
+            shape="round"
+            icon={<ArrowLeftOutlined />}
+            size="small"
+          >
+            Home
+          </Button>
+        </Link>
+      </div>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button
+            type="primary"
+            htmlType="submit"
+            style={{ background: "aqua" }}
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+export default Login;
--- week17/easy-hr/src/features/home/Home.js
+++ week17/easy-hr/src/features/home/Home.js
@@ -1,58 +1,66 @@
 import "./Home.scss";
 
-import { Col, Row } from "antd";
+import { useDispatch, useSelector } from "react-redux";
 
 import { ArrowRightOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import { Link } from "react-router-dom";
 
-const Home = () => (
-  <div className="home">
-    <header>
-      <Link className="logo" to="/">
-        EasyHR
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-    <main>
-      <div className="description">
-        <div className="description-heading">
-          <p>
-            Welcome to <span>EasyHr.</span> <br />
-            Your Employee Management System
-          </p>
+const Home = () => {
+  const { isLoggedIn } = useSelector((state) => state.auth);
+
+  return (
+    <div className="home">
+      <header>
+        <Link className="logo" to="/">
+          EasyHR
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            {!isLoggedIn ? (
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            ) : null}
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <div className="description">
+          <div className="description-heading">
+            <p>
+              Welcome to <span>EasyHr.</span> <br />
+              Your Employee Management System
+            </p>
+          </div>
+          <div className="description-text">
+            <p>Easily manage your employees without all the hastle</p>
+          </div>
+          <div className="description-action">
+            <Link to="/dashboard">
+              <Button
+                type="primary"
+                style={{ background: "aqua" }}
+                shape="round"
+                icon={<ArrowRightOutlined />}
+                size="large"
+              >
+                Dashboard
+              </Button>
+            </Link>
+          </div>
         </div>
-        <div className="description-text">
-          <p>Easily manage your employees without all the hastle</p>
+        <div className="home-img">
+          <img src="/management.jpg" alt="management" />
         </div>
-        <div className="description-action">
-          <Button
-            type="primary"
-            style={{ background: "aqua" }}
-            shape="round"
-            icon={<ArrowRightOutlined />}
-            size="large"
-          >
-            Download
-          </Button>
-        </div>
-      </div>
-      <div className="home-img">
-        <img src="/management.jpg" alt="management" />
-      </div>
-    </main>
-  </div>
-);
+      </main>
+    </div>
+  );
+};
 export default Home;
